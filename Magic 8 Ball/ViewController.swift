c2e476867617720979557786b3409e0ac1f0b498
bijan102 <bfazeli1@gmail.com>
2017-11-29 16:32:50 -0800

    Linked the design with code and made the ball image appear w/ a random omg every time app is first loaded

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Array to hold all the different ball images
+    var ballArray : [String] = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    var randomBallNumber : Int = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        randomBallNumber = Int(arc4random_uniform(4))
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        
+    }
+    
 }
 
